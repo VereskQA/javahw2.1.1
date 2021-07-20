6c25b00a86204eae2346c03dd48f1739ca762855
Ivan Kalashnikov <ik.veresk@gmail.com>
2021-07-20 09:09:56 +0300

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 7631;
+        int oneMile = 20;
+        int bonusMile = ticketPrice / oneMile;
+        System.out.println(bonusMile);
+    }
+}
